754d8b3975aa95788792b539f48e977afc0d7659
ronihacohen <roni@alooma.io>
2017-09-18 13:00:42 +0300

    Adding resize script

---
+++ assets/images/resize.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const sharp = require("sharp");
+
+function readFiles(dirname, onError) {
+  fs.readdir(dirname, function(err, filenames) {
+    if (err) {
+      onError(err);
+      return;
+    }
+    filenames.forEach(function(filename) {
+      fs.readFile(dirname + filename, function(err, content) {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        console.log(filename);
+        sharp(content)
+          .resize(320, 240)
+          .toFile(filename, (err, info) => console.log(err, info));
+      });
+    });
+  });
+}
+
+readFiles("./original/");
