9cf9b395b6f23935322f6b8030ccd17e94d2504c
ronihacohen <roni@alooma.io>
2017-09-21 08:16:47 +0300

    Create answers file automatically

--- assets/images/resize.js
+++ assets/images/resize.js
@@ -1,6 +1,32 @@
 const fs = require("fs");
 const sharp = require("sharp");
 
+const ANSWER_FILE = "../../answers.js";
+
+fs.unlink(ANSWER_FILE, err => {
+  if (err) console.log("Can't delete answer file.");
+});
+
+fs.appendFileSync(ANSWER_FILE, `export default [`);
+
+function addAnswer(name, filename) {
+  if (!name) {
+    return;
+  }
+  fs.appendFileSync(
+    ANSWER_FILE,
+    `
+    {
+      title: "${name}",
+      sound: require("./assets/sounds/${name}.m4a"),
+      image: require("./assets/images/${filename}")
+    },`,
+    err => {
+      if (err) throw err;
+    }
+  );
+}
+
 function readFiles(dirname, onError) {
   fs.readdir(dirname, function(err, filenames) {
     if (err) {
@@ -8,17 +34,27 @@ function readFiles(dirname, onError) {
       return;
     }
     filenames.forEach(function(filename) {
+      const filenameArray = filename.split(".");
+      const name = filenameArray[0];
+      const extension = filenameArray[1];
+      if (extension !== "jpg" && extension !== "jpeg") {
+        return;
+      }
+      addAnswer(name, filename);
       fs.readFile(dirname + filename, function(err, content) {
         if (err) {
           console.log(err);
           return;
         }
-        console.log(filename);
+        console.log(`${name} was added.`);
         sharp(content)
           .resize(150, 150)
-          .toFile(filename, (err, info) => console.log(err, info));
+          .toFile(filename, (err, info) => {
+            if (err) console.log(err);
+          });
       });
     });
+    fs.appendFileSync(ANSWER_FILE, `];`);
   });
 }
 
