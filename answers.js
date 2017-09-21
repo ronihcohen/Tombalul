55543c84f5c2637aabf2a6fadd355dd5323a2f78
ronihacohen <roni@alooma.io>
2017-09-21 12:04:33 +0300

    Adding new sounds

--- answers.js
+++ answers.js
@@ -1,117 +1,111 @@
 export default [
-  {
-    title: "bear",
-    sound: require("./assets/sounds/bear.m4a"),
-    image: require("./assets/images/bear.jpeg")
-  },
-  {
-    title: "bee",
-    sound: require("./assets/sounds/bee.m4a"),
-    image: require("./assets/images/bee.jpeg")
-  },
-  {
-    title: "beetle",
-    sound: require("./assets/sounds/beetle.m4a"),
-    image: require("./assets/images/beetle.jpeg")
-  },
-  {
-    title: "bird",
-    sound: require("./assets/sounds/bird.m4a"),
-    image: require("./assets/images/bird.jpg")
-  },
-  {
-    title: "cat",
-    sound: require("./assets/sounds/cat.m4a"),
-    image: require("./assets/images/cat.jpg")
-  },
-  {
-    title: "dog",
-    sound: require("./assets/sounds/dog.m4a"),
-    image: require("./assets/images/dog.jpg")
-  },
-  {
-    title: "dolphin",
-    sound: require("./assets/sounds/dolphin.m4a"),
-    image: require("./assets/images/dolphin.jpeg")
-  },
-  {
-    title: "duck",
-    sound: require("./assets/sounds/duck.m4a"),
-    image: require("./assets/images/duck.jpeg")
-  },
-  {
-    title: "elephant",
-    sound: require("./assets/sounds/elephant.m4a"),
-    image: require("./assets/images/elephant.jpg")
-  },
-  {
-    title: "fish",
-    sound: require("./assets/sounds/fish.m4a"),
-    image: require("./assets/images/fish.jpeg")
-  },
-  {
-    title: "giraffe",
-    sound: require("./assets/sounds/giraffe.m4a"),
-    image: require("./assets/images/giraffe.jpeg")
-  },
-  {
-    title: "hippopotamus",
-    sound: require("./assets/sounds/hippopotamus.m4a"),
-    image: require("./assets/images/hippopotamus.jpeg")
-  },
-  {
-    title: "horse",
-    sound: require("./assets/sounds/horse.m4a"),
-    image: require("./assets/images/horse.jpg")
-  },
-  {
-    title: "lion",
-    sound: require("./assets/sounds/lion.m4a"),
-    image: require("./assets/images/lion.jpg")
-  },
-  {
-    title: "monkey",
-    sound: require("./assets/sounds/monkey.m4a"),
-    image: require("./assets/images/monkey.jpg")
-  },
-  {
-    title: "mouse",
-    sound: require("./assets/sounds/mouse.m4a"),
-    image: require("./assets/images/mouse.jpg")
-  },
-  {
-    title: "owl",
-    sound: require("./assets/sounds/owl.m4a"),
-    image: require("./assets/images/owl.jpeg")
-  },
-  {
-    title: "pig",
-    sound: require("./assets/sounds/pig.m4a"),
-    image: require("./assets/images/pig.jpeg")
-  },
-  {
-    title: "rabbit",
-    sound: require("./assets/sounds/rabbit.m4a"),
-    image: require("./assets/images/rabbit.jpg")
-  },
-  {
-    title: "sheep",
-    sound: require("./assets/sounds/sheep.m4a"),
-    image: require("./assets/images/sheep.jpeg")
-  },
-  {
-    title: "snake",
-    sound: require("./assets/sounds/snake.m4a"),
-    image: require("./assets/images/snake.jpeg")
-  },
-  {
-    title: "tiger",
-    sound: require("./assets/sounds/tiger.m4a"),
-    image: require("./assets/images/tiger.jpeg")
-  },
-  {
-    title: "zebra",
-    sound: require("./assets/sounds/zebra.m4a"),
-    image: require("./assets/images/zebra.jpeg")
-  }
-];
+    {
+      title: "bear",
+      sound: require("./assets/sounds/bear.m4a"),
+      image: require("./assets/images/bear.jpeg")
+    },
+    {
+      title: "bee",
+      sound: require("./assets/sounds/bee.m4a"),
+      image: require("./assets/images/bee.jpeg")
+    },
+    {
+      title: "beetle",
+      sound: require("./assets/sounds/beetle.m4a"),
+      image: require("./assets/images/beetle.jpeg")
+    },
+    {
+      title: "bird",
+      sound: require("./assets/sounds/bird.m4a"),
+      image: require("./assets/images/bird.jpg")
+    },
+    {
+      title: "cat",
+      sound: require("./assets/sounds/cat.m4a"),
+      image: require("./assets/images/cat.jpg")
+    },
+    {
+      title: "dog",
+      sound: require("./assets/sounds/dog.m4a"),
+      image: require("./assets/images/dog.jpg")
+    },
+    {
+      title: "dolphin",
+      sound: require("./assets/sounds/dolphin.m4a"),
+      image: require("./assets/images/dolphin.jpeg")
+    },
+    {
+      title: "duck",
+      sound: require("./assets/sounds/duck.m4a"),
+      image: require("./assets/images/duck.jpeg")
+    },
+    {
+      title: "elephant",
+      sound: require("./assets/sounds/elephant.m4a"),
+      image: require("./assets/images/elephant.jpg")
+    },
+    {
+      title: "fish",
+      sound: require("./assets/sounds/fish.m4a"),
+      image: require("./assets/images/fish.jpeg")
+    },
+    {
+      title: "giraffe",
+      sound: require("./assets/sounds/giraffe.m4a"),
+      image: require("./assets/images/giraffe.jpeg")
+    },
+    {
+      title: "hippopotamus",
+      sound: require("./assets/sounds/hippopotamus.m4a"),
+      image: require("./assets/images/hippopotamus.jpeg")
+    },
+    {
+      title: "horse",
+      sound: require("./assets/sounds/horse.m4a"),
+      image: require("./assets/images/horse.jpg")
+    },
+    {
+      title: "lion",
+      sound: require("./assets/sounds/lion.m4a"),
+      image: require("./assets/images/lion.jpg")
+    },
+    {
+      title: "monkey",
+      sound: require("./assets/sounds/monkey.m4a"),
+      image: require("./assets/images/monkey.jpg")
+    },
+    {
+      title: "mouse",
+      sound: require("./assets/sounds/mouse.m4a"),
+      image: require("./assets/images/mouse.jpg")
+    },
+    {
+      title: "owl",
+      sound: require("./assets/sounds/owl.m4a"),
+      image: require("./assets/images/owl.jpeg")
+    },
+    {
+      title: "pig",
+      sound: require("./assets/sounds/pig.m4a"),
+      image: require("./assets/images/pig.jpeg")
+    },
+    {
+      title: "rabbit",
+      sound: require("./assets/sounds/rabbit.m4a"),
+      image: require("./assets/images/rabbit.jpg")
+    },
+    {
+      title: "sheep",
+      sound: require("./assets/sounds/sheep.m4a"),
+      image: require("./assets/images/sheep.jpeg")
+    },
+    {
+      title: "snake",
+      sound: require("./assets/sounds/snake.m4a"),
+      image: require("./assets/images/snake.jpeg")
+    },
+    {
+      title: "zebra",
+      sound: require("./assets/sounds/zebra.m4a"),
+      image: require("./assets/images/zebra.jpeg")
+    },];
